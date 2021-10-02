411c70d28216da233c200071713c261602f06134
jabali2004 <32802935+jabali2004@users.noreply.github.com>
2021-10-02 20:17:09 +0200

    update public-api.ts

--- projects/ngx-formulus/src/public-api.ts
+++ projects/ngx-formulus/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-formulus
  */
 
-export * from './lib/ngx-formulus.service';
-export * from './lib/ngx-formulus.component';
-export * from './lib/ngx-formulus.module';
+//  Modules
+export { NgxFormulusModule } from './lib/ngx-formulus.module';
