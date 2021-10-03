2c21c739790e5ac1b8806f781258dedca027915f
jabali2004 <32802935+jabali2004@users.noreply.github.com>
2021-10-03 16:38:38 +0200

    chore: add basic example

--- projects/ngx-formulus-example/src/app/app.component.ts
+++ projects/ngx-formulus-example/src/app/app.component.ts
@@ -1,10 +1,33 @@
+import type { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ControlType } from 'projects/ngx-formulus/src/lib/enums/control-type';
+import type { DynamicForm } from 'projects/ngx-formulus/src/lib/types/dynamic-form';
+import type { DynamicType } from 'projects/ngx-formulus/src/lib/types/dynamic-type';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ngx-formulus-example';
+export class AppComponent implements OnInit {
+  public title = 'ngx-formulus-example';
+
+  public formGroup: FormGroup = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    birthDate: new FormControl('', Validators.required),
+  });
+
+  public dynamicForm: DynamicForm = {
+    formGroup: this.formGroup,
+    config: {
+      controls: [
+        { control: 'firstName', label: 'First Name' },
+        { control: 'birthDate', label: 'Birth Date', type: ControlType.Date },
+      ],
+    },
+  };
+
+  ngOnInit(): void {}
 }
